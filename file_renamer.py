8296d6f631752877c4d0f05411e8374ea8ae6a20
mohammadSHP <85444253+mohammadSHP@users.noreply.github.com>
2021-11-09 16:12:59 +0330

    Add files via upload

---
+++ file_renamer.py
@@ -0,0 +1,22 @@
+import os
+
+folder = r'C:\\Users\\mshar\\Desktop\\Custom haarcascade\\p\\New folder\\'
+count = 1
+# count increase by 1 in each iteration
+# iterate all files from a directory
+for file_name in os.listdir(folder):
+    # Construct old file name
+    source = folder + file_name
+
+    # Adding the count to the new file name and extension
+    destination = folder + "nose_" + str(count) + ".jpg"
+
+    # Renaming the file
+    os.rename(source, destination)
+    count += 1
+print('All Files Renamed')
+
+print('New Names are')
+# verify the result
+res = os.listdir(folder)
+print(res)
